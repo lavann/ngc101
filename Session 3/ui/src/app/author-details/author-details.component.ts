6d6f65bf4483f4e9bb402d4d256835dc5b83e01e
Lavan <61334851+lanallai@users.noreply.github.com>
2020-10-02 12:08:53 +0100

    fixed error in api, read notes in startup.cs

--- Session 3/ui/src/app/author-details/author-details.component.ts
+++ Session 3/ui/src/app/author-details/author-details.component.ts
@@ -6,37 +6,45 @@ import { Author } from '../models/author';
 @Component({
   selector: 'app-author-details',
   templateUrl: './author-details.component.html',
-  styleUrls: ['./author-details.component.css']
+  styleUrls: ['./author-details.component.css'],
 })
 export class AuthorDetailsComponent implements OnInit {
-
   genre: string;
   id: string;
   author: Author;
-  constructor(private route: ActivatedRoute, private authorService: AuthorsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private authorService: AuthorsService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-
+    this.route.params.subscribe((params) => {
       this.id = params['id'];
       this.genre = params['genre'];
 
-      this.authorService.returnAuthorByGenreAndId(this.genre, this.id).subscribe(resp => {
-        console.log(resp);
-        this.author = resp;
-      }, error => {
-        console.log(error)
-      })
+      this.authorService
+        .returnAuthorByGenreAndId(this.genre, this.id)
+        .subscribe(
+          (resp) => {
+            console.log(resp);
+            this.author = resp;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     });
   }
 
   updateAuthor() {
-    this.authorService.updateAuthor(this.author).subscribe(response => {
-      console.log(response);
-      this.author = response;
-    }, error => {
-      console.log(error);
-    })
+    this.authorService.updateAuthor(this.author).subscribe(
+      (response) => {
+        console.log(response);
+        this.author = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
